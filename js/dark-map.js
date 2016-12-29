efded2f2aaba7282cc8b9cb40289a508b8f62463
Jason <jasonwhite1976@yahoo.co.uk>
2016-12-29 08:52:57 +0000

    update template for page launch

--- js/dark-map.js
+++ js/dark-map.js
@@ -11,8 +11,8 @@
 			var map;
 			map = new GMaps({
 				el: '#map',
-				lat: 30.609788,			// Change to your location's latitude
-				lng: 32.268555,			// Change to your location's longitude
+				lat: 52.2178,			// Change to your location's latitude
+				lng: -0.0662,			// Change to your location's longitude
 				scrollwheel:false,
 				zoom: 17,
 				zoomControl : true,
@@ -24,7 +24,7 @@
 			});
 
 			var image = '';
-			map.addMarker({
+			/*map.addMarker({
 				lat: 30.609788,			// Change to your location's latitude
 				lng: 32.268555,			// Change to your location's longitude
 				icon: image,
@@ -32,14 +32,14 @@
 				verticalAlign: 'bottom',
 				horizontalAlign: 'center',
 				backgroundColor: '#d3cfcf',
-			});
+			});*/
 
 			var styles = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
 
 			map.addStyle({
 			styledMapName:"Styled Map",
 			styles: styles,
-			mapTypeId: "map_style"  
+			mapTypeId: "map_style"
 			});
 
 			map.setStyle("map_style");
